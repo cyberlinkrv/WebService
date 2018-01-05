7bcb392d01b5226c48b0efe137e38f86dfdef46b
cyberlinkrv <divino_j_silva@hotmail.com>
2018-01-05 16:42:49 -0200

    Removed comments on the connector that are not needed for the project

--- src/java/br/com/cyberlinkrv/conector/conectorMySQL.java
+++ src/java/br/com/cyberlinkrv/conector/conectorMySQL.java
@@ -6,12 +6,8 @@ import java.sql.SQLException;
 
 public class conectorMySQL {
 
-        private static final String Url = "jdbc:mysql://10.211.55.5:3306/Loja_Expert";
-	//private static final String Url = "jdbc:mysql://localhost:3306/Loja_Expert";
-        //private static final String Url="jdbc:mysql://loja_expert.mysql.dbaas.com.br/loja_expert";
+        private static final String Url = "jdbc:mysql://10.211.55.5:3306/Loja_Expert"; //this for use in local network
 	private static final String User = "root";
-        //private static final String User = "loja_expert";
-	//private static final String Password = "djs052190";
         private static final String Password = "052190";
 
 	public static Connection obterConexao() throws SQLException {
