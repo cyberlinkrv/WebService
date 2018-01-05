bda11986492785525da28104b2d44fa4e350e9a6
cyberlinkrv <divino_j_silva@hotmail.com>
2018-01-05 16:56:36 -0200

    end

--- src/java/br/com/cyberlinkrv/conector/conectorMySQL.java
+++ src/java/br/com/cyberlinkrv/conector/conectorMySQL.java
@@ -8,7 +8,7 @@ public class conectorMySQL {
 
         private static final String Url = "jdbc:mysql://10.211.55.5:3306/Loja_Expert"; //this for use in local network
 	private static final String User = "root";
-        private static final String Password = "052190";
+        private static final String Password = "052190";//
 
 	public static Connection obterConexao() throws SQLException {
 
